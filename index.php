dc8d75098d160ac80521223a3ef880dd0825bf20
valen19 <valenbautistao38@gmail.com>
2023-07-17 14:30:16 -0500

    El commit de saray

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>VISION VIAJERA</title>
+	<link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+	<header>
+		<nav class="menu">
+			<h1 class="titulo">VISION VIAJERA</h1>
+			<ul>
+				<li><a href="#">Inicio</a></li>
+				<li><a href="#">Nosotros</a></li>
+				<li><a href="#">Imagenes</a></li>
+				<li><a href="#">Lugares Turisticos</a></li>
+				<li><a href="#">Contacto</a></li>
+			</ul>
+		</nav>
+		
+	</header>
+	<main>
+		
+	</main>
+
+</body>
+</html
